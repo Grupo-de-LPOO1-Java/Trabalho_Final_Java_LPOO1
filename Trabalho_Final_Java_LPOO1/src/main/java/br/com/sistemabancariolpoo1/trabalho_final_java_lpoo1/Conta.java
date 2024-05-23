f72951fdc84341e794723262ea797c57939ccd3c
Rafael Cecyn <rafael.mendes112@hotmail.com>
2024-05-23 20:52:56 -0300

    Inserindo interface ContaI e classe abstrata Conta

---
+++ Trabalho_Final_Java_LPOO1/src/main/java/br/com/sistemabancariolpoo1/trabalho_final_java_lpoo1/Conta.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.sistemabancariolpoo1.trabalho_final_java_lpoo1;
+
+/**
+ *
+ * @author rafae
+ */
+public abstract class Conta implements ContaI {
+    private int numeroConta;
+    private double saldo;
+    private Cliente cliente;
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+    
+    
+    
+}
