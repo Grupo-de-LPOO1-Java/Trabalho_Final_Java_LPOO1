90034b30f1bcf508fca480b1d7a946e14b6e07b7
Rafael Cecyn <rafael.mendes112@hotmail.com>
2024-05-23 19:58:24 -0300

    Criacao da classe principal

---
+++ Trabalho_Final_Java_LPOO1/src/main/java/br/com/sistemabancariolpoo1/trabalho_final_java_lpoo1/Trabalho_Final_Java_LPOO1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.sistemabancariolpoo1.trabalho_final_java_lpoo1;
+
+/**
+ *
+ * @author rafae
+ */
+public class Trabalho_Final_Java_LPOO1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
