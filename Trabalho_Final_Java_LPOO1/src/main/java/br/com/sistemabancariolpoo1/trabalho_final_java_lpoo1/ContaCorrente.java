957f8bb4ecd634347ad7f12b095b6c1c6616479a
João Paes <126814093+xuizi@users.noreply.github.com>
2024-05-23 21:01:05 -0300

    criacao da classe ContaCorrente e ContaInvestimento

---
+++ Trabalho_Final_Java_LPOO1/src/main/java/br/com/sistemabancariolpoo1/trabalho_final_java_lpoo1/ContaCorrente.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.sistemabancariolpoo1.trabalho_final_java_lpoo1;
+
+/**
+ *
+ * @author joaop
+ */
+public class ContaCorrente extends Conta {
+    private double depositoInicial;
+    private double limit;
+
+    public double getDepositoInicial() {
+        return depositoInicial;
+    }
+
+    public void setDepositoInicial(double depositoInicial) {
+        this.depositoInicial = depositoInicial;
+    }
+
+    public double getLimit() {
+        return limit;
+    }
+
+    public void setLimit(double limit) {
+        this.limit = limit;
+    }
+    
+    
+
+}
---
+++ Trabalho_Final_Java_LPOO1/src/main/java/br/com/sistemabancariolpoo1/trabalho_final_java_lpoo1/ContaInvestimento.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.sistemabancariolpoo1.trabalho_final_java_lpoo1;
+
+/**
+ *
+ * @author joaop
+ */
+public class ContaInvestimento extends Conta {
+    private double montanteMinimo;
+    private double depositoMinimo;
+    private double depositoInicial;
+
+    public double getMontanteMinimo() {
+        return montanteMinimo;
+    }
+
+    public void setMontanteMinimo(double montanteMinimo) {
+        this.montanteMinimo = montanteMinimo;
+    }
+
+    public double getDepositoMinimo() {
+        return depositoMinimo;
+    }
+
+    public void setDepositoMinimo(double depositoMinimo) {
+        this.depositoMinimo = depositoMinimo;
+    }
+
+    public double getDepositoInicial() {
+        return depositoInicial;
+    }
+
+    public void setDepositoInicial(double depositoInicial) {
+        this.depositoInicial = depositoInicial;
+    }
+    
+    
+}
+
