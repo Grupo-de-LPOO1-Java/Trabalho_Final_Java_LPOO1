3b648ced8cf86fe20c9fa1d4d421952d4ed33c0c
Rafael Cecyn <rafael.mendes112@hotmail.com>
2024-11-13 20:49:38 -0300

    Implementando a classe Connection Factory

---
+++ Trabalho_Final_Java_LPOO1/src/main/java/br/com/sistemabancariolpoo1/trabalho_final_java_lpoo1/ConnectionFactory.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.sistemabancariolpoo1.trabalho_final_java_lpoo1;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.sql.Connection;
+
+/**
+ *
+ * @author rafae
+ */
+public class ConnectionFactory {
+    
+    private static Properties properties;
+    
+    private ConnectionFactory() {   
+    }
+    
+    public static Connection getConnection() throws SQLException, IOException {
+        readProperties();
+        String url = properties.getProperty("db.url");
+        String user = properties.getProperty("db.user");
+        String pwd = properties.getProperty("db.pwd");
+        return DriverManager.getConnection(url, user, pwd);
+    }
+    
+    
+    private static void readProperties() throws IOException {
+        if(properties==null) {
+            Properties props = new Properties();
+            FileInputStream file = new FileInputStream(
+            "C:\\Users\\rafae\\OneDrive\\Documentos\\NetBeansProjects\\Trabalho_Final_Java_LPOO1\\Trabalho_Final_Java_LPOO1\\src\\main\\java\\br\\com\\sistemabancariolpoo1\\trabalho_final_java_lpoo1\\DataBase.properties");
+            props.load(file);
+            properties = props;
+        }
+    }
+            
+    
+}
