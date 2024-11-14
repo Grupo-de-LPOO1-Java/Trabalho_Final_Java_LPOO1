14b6587922a31cda31ede7cb6840c59fdcc3510e
Rafael Cecyn <rafael.mendes112@hotmail.com>
2024-11-13 22:56:15 -0300

    Implementando a classe ClienteDaoSql e alterando interface Dao

--- Trabalho_Final_Java_LPOO1/src/main/java/br/com/sistemabancariolpoo1/trabalho_final_java_lpoo1/Dao.java
+++ Trabalho_Final_Java_LPOO1/src/main/java/br/com/sistemabancariolpoo1/trabalho_final_java_lpoo1/Dao.java
@@ -14,7 +14,7 @@ import java.util.List;
 public interface Dao<T> {
     public void add(T objeto) throws Exception; 
     public List<T> getAll() throws Exception;
-    public T getById(long id) throws Exception;
+    public T getByCPF(String cpf) throws Exception;
     public void update(T objeto) throws Exception;
     public void delete(T objeto) throws Exception;
     public void deleteAll() throws Exception;
